552629f6138df4cce41c6af464b99a9ab1f6d47d
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2022-10-14 01:27:09 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,28 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - function with two arguments
- * @n: int type first argume t
- *
- * Description: returns the sum of all its parameters
- * Return: value of sum or 0
- */
+* sum_them_all - Will sum a variable number of parameters
+* @n: Amount of parameters
+* Return: Sum of all parameters
+**/
+
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
-	int sum = 0;
-	va_list arg;
-
-	if (n == 0)
-		return (0);
-	va_start(arg, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(arg, int);
-	}
-	va_end(arg);
-	return (sum);
+va_list ap; /** Macro that defines a list of argument paramenters */
+unsigned int i = 0; /** Counter */
+int sum = 0; /** Resulting value of the sum */
+if (n == 0) /** Will check if is or is not reciving parameters */
+{
+return (0); /** Will return 0 is has no values */
+}
+else
+{
+va_start(ap, n); /** Initializes the use of the list */
+for (i = 0; i < n; i++) /** Loops the function to sum parameters */
+{
+sum += va_arg(ap, int); /** Adds the list of argument parameters to the sum */
+}
+va_end(ap); /** Marker wich defines the end of use of the list */
+}
+return (sum); /** Will return the resulting value of the sum */
 }
